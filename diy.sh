2debdd0b4aaebe63e81aaf60e76a904370c5e969
sangoking <60558177+sangoking@users.noreply.github.com>
2020-02-28 14:29:06 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,14 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#自定插件
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial package/luci-theme-atmaterial
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/Lienol/openwrt-package/tree/master/lienol/luci-app-passwall package/luci-app-passwall
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+git clone https://github.com/lisaac/luci-app-diskman 
+
+#更新feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
